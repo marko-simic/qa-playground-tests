7494e3d6071e35edc594a639b3824f863c35f0f1
marko-simic <marko.simic@outlook.com>
2022-03-04 11:59:21 +0100

    Completed tests

--- tests/hidden.spec.js
+++ tests/hidden.spec.js
@@ -1,12 +1,14 @@
 const { test, expect } = require("@playwright/test");
 
-test.describe.only("Hidden Elements", () => {
+test.describe("Hidden Elements", () => {
   test("Should click on the hidden button", async ({ page }) => {
     await page.goto("apps/outside-viewport/");
     await expect(page.locator("text=Click the button below")).toBeVisible();
 
-    await page.locator("#fugitive").click();
+    const infoMsg = page.locator("#info"); // Select paragraph element
 
-    await page.pause();
+    await page.locator("#fugitive").click(); // Click on the button
+
+    await expect(infoMsg).toHaveText("Mission accomplished"); // Assert changed message
   });
 });
--- tests/onboarding.spec.js
+++ tests/onboarding.spec.js
@@ -1,9 +1,19 @@
 const { test, expect } = require("@playwright/test");
 
-test.describe.only("Onboarding", () => {
+test.describe("Onboarding", () => {
   test("Should close onboarding modal if it exists", async ({ page }) => {
     await page.goto("apps/onboarding-modal/");
+    await expect(page.locator(".title")).toBeVisible();
 
-    await page.pause();
+    const title = page.locator(".title");
+    const modal = await page.locator("#active").isChecked();
+    const closeButton = page.locator('[for="active"]');
+
+    if (modal) {
+      await closeButton.click();
+      await expect(title).toHaveText("Welcome Peter Parker! 🕷🎉");
+    } else {
+      await expect(title).toHaveText("Application successfully launched! 🚀");
+    }
   });
 });
