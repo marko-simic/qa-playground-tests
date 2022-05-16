264053b5a8e6cf03cdf1408bd20ca3650bd0373c
marko-simic <marko.simic@outlook.com>
2022-05-16 23:07:25 +0200

    Experiment

---
+++ global-teardown.js
@@ -0,0 +1,15 @@
+import * as core from "@actions/core";
+await core.summary
+  .addHeading("Test Results")
+  .addCodeBlock(generateTestResults(), "js")
+  .addTable([
+    [
+      { data: "File", header: true },
+      { data: "Result", header: true },
+    ],
+    ["foo.js", "Pass "],
+    ["bar.js", "Fail "],
+    ["test.js", "Pass "],
+  ])
+  .addLink("View staging deployment!", "https://github.com")
+  .write();
--- playwright.config.js
+++ playwright.config.js
@@ -28,6 +28,8 @@ const config = {
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+  // This file is required and run after all the tests
+  globalTeardown: process.env.CI ? require.resolve("./global-teardown") : "",
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [["list"], ["html"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
