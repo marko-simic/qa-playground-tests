0f8499b7c552c369e002bcf1a64cd80cef7d1a37
Marko Simic <marko.simic@outlook.com>
2026-02-15 19:17:44 +0100

    Updated Playwright and fixed the fetch test

--- tests/fetch.spec.js
+++ tests/fetch.spec.js
@@ -1,12 +1,14 @@
-const { test, expect } = require("@playwright/test");
+const { test, expect } = require("@playwright/test")
 
 test.describe("Fetching Data", () => {
   test("Should load and display all posts", async ({ page }) => {
-    await page.goto("apps/fetch/", { waitUntil: "commit" });
+    // Wait for JSON
+    const jsonResponsePromise = page.waitForResponse((resp) =>
+      resp.url().includes("/posts"),
+    )
 
-    await page.waitForResponse((resp) => resp.url().includes("/posts")); // Wait for JSON
-    await page.waitForResponse("https://images.unsplash.com/**"); // Wait for image
+    await Promise.all([page.goto("apps/fetch/"), jsonResponsePromise])
 
-    expect(await page.locator(".icard").count()).toBeGreaterThan(90);
-  });
-});
+    expect(await page.locator(".icard").count()).toBeGreaterThan(90)
+  })
+})
